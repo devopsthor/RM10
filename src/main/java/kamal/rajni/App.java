31fd45dcbb56600d1db70c36d7a6c46aedf801d7
Ramganesh <Kamalhassan123@gmail.com>
2024-06-13 12:43:08 +0530

    MV JAR

---
+++ src/main/java/kamal/rajni/App.java
@@ -0,0 +1,13 @@
+package kamal.rajni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
